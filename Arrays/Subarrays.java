d30a95922c4ec0c5544280d8c935c6049e7b4382
gaurav gandhi <grv.gandhi.27@gmail.com>
2020-05-21 09:10:54 +0530

    Subarray with atleast k sum question is solved.

--- Arrays/Subarrays.java
+++ Arrays/Subarrays.java
@@ -1,15 +1,43 @@
+import java.io.*;
+import java.util.*;
+
 public class Subarrays {
     public static void main(String args[]) {
-        int arr[] = {1,2,3,4,5,6,7,8,9,10};
-        int n = 10;
-        int i,j,k;
-        for(i=0;i<n;i++) {
-            for(j=1;(i+j)<=n;j++) {
-                for(k=i;k<i+j;k++) {
-                    System.out.print(arr[k]+ " ");
-                }
-                System.out.println();
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- != 0) {
+            int n = sc.nextInt();
+            int kk = sc.nextInt();
+            int arr[] = new int[n];
+            int i, k;
+            for (i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
             }
+            int sum =0 ;
+            k = 0;
+            int ans = 0;
+            for(i =0 ;i<n;i++) {
+
+                while(sum < kk) {
+                    if(k== n) {
+                        break;
+                    }
+                    else {
+                        sum = sum + arr[k];
+                        k++;
+                    }
+                }
+                
+                if(sum < kk) {
+                    break;
+                }
+
+                ans = ans + n - k + 1;
+                sum = sum - arr[i];
+            }
+
+            System.out.println(ans);
         }
-    }    
+        sc.close();
+    }
 }
